1a6cf869c765f1bdd8a304685dc07de910361911
Krocketeer <Krocketeer@github.com>
2020-08-06 12:41:38 -0700

    forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Formy from "./Form"
+import * as yup from "yup"
+
+const loginForm=[
+    {name:'username', type:'text'},
+    {name:'password', type:'password'},
+    {name:'phone', type:'text'}
+]
+
+const eventForm=[
+    {name:'name', type:'text'},
+    {name:'date', type:'date'}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h1>login form</h1>
+          <Formy schema={loginForm} />
+          <br/><br/><br/>
+          <h1>create event form</h1>
+          <Formy schema={eventForm} />
+      </div>
   );
 }
 
---
+++ src/Form.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import { Formik, Form, ErrorMessage } from 'formik';
+import {InputGroup} from "@blueprintjs/core";
+
+function Formy({schema}) {
+    return <Formik
+        initialValues={{ username: '', password: '' }}
+        validate={values => {
+            const errors = {};
+            if (!values.username) {
+                errors.username = 'Username must be at least 6 characters';
+            } else if (values.username.length<6) {
+                errors.username = 'Username too short';
+            }
+            return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+            }, 400);
+        }}
+    >
+        {({ isSubmitting, setFieldValue, onBlur, onChange, errors }) => {
+            return <Form style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                {schema.map((f, i) => {
+                    return <Field key={i} type={f.type}
+                                  name={f.name} onBlur={onBlur}
+                                  onChange={e => setFieldValue(f.name, e.target.value)}
+                                  error={errors[f.name]}
+                    />
+                })}
+                <button type="submit" disabled={isSubmitting}>
+                    Submit
+                </button>
+            </Form>
+        }}
+    </Formik>
+}
+
+function Field({type,name,onChange,onBlur,error}){
+    return <div style={{margin:'10px 0'}}>
+        <InputGroup
+            name={name}
+            type={type}
+            placeholder={'Enter your '+name}
+            onChange={onChange}
+            onBlur={onBlur}
+        />
+        <div style={{color:'red'}}>{error}</div>
+    </div>
+}
+
+export default Formy
